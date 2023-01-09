70e3a40caf3c6f48271c39f1d3e054b0c30405d7
typical <85001520+TypicallyAUser@users.noreply.github.com>
2023-01-09 18:32:32 -0500

    reset ur scripts

---
+++ reset.lua
@@ -0,0 +1,8 @@
+print("Resetting scripts...")
+
+writefile("BootlegData.json",game:GetService("HttpService"):JSONEncode({
+	{Name="Add Script",Desc="A simple GUI to add scripts to the command bar.",Script="addscript.lua"}
+	{Name="Reset Scripts",Desc="A permanent action! Do not use unless you absolutely want to!",Script="https://raw.githubusercontent.com/TypicallyAUser/bootleg/main/reset.lua",IsLoadstring=true}
+}))
+
+print("Resetted!")
