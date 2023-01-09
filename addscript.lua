0ed39905dc07c7a9af2ba392c0b2d1a2dab640e6
typical <85001520+TypicallyAUser@users.noreply.github.com>
2023-01-09 18:56:06 -0500

    fixed a stupid issue with the reset thing

--- addscript.lua
+++ addscript.lua
@@ -1,11 +1,12 @@
 if not isfile("BootlegData.json") then
 writefile("BootlegData.json",game:GetService("HttpService"):JSONEncode({
-	{Name="Add Script",Desc="A simple GUI to add scripts to the command bar.",Script="addscript.lua"}
+	{Name="Add Script",Desc="A simple GUI to add scripts to the command bar.",Script="addscript.lua"};
+	{Name="Reset Scripts",Desc="A permanent action! Do not use unless you absolutely want to!",Script="https://raw.githubusercontent.com/TypicallyAUser/bootleg/main/reset.lua",IsLoadstring=true};
 }))
 end
 if not isfolder("bootleg") then
 	makefolder("bootleg")
-end 
+end
 
 local data = game:GetService("HttpService"):JSONDecode(readfile('BootlegData.json'))
 
